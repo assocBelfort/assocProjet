68ab26189ffe81a50fe3df2dcc6a2f929fdcb2ca
Jampard <jlounes@gmail.com>
2012-06-14 12:07:06 +0200

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 
 class UsersController < ApplicationController
   def new
+  	@user = User.new
+  end
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		flash[:success] = "Bienvenue hataillure"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+  def show
+  	@user = User.find(params[:id])
   end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Lounes Jaafar"
+    email    "jafton@tinton.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
